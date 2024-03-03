69a5e512912871822288176f8aa67968eb44363d
Mikhail Dyagilev <m.dyagilev@yadro.com>
2024-03-03 21:33:56 +0300

    done 3.1

--- lab03/lab03_A/cmd/ws_server/main.go
+++ lab03/lab03_A/cmd/ws_server/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
@@ -17,7 +16,6 @@ func main() {
 	port := fmt.Sprintf(":%v", *portFromInput)
 	listener, err := net.Listen("tcp", port)
 	if err != nil {
-
 		log.Printf("error while sw server starting: %v", err)
 
 		return
@@ -26,7 +24,6 @@ func main() {
 	defer listener.Close()
 
 	log.Printf("Server is up on port%v", port)
-
 	conn, err := listener.Accept()
 	if err != nil {
 		log.Printf("error while adding new connection: %v", err)
@@ -35,20 +32,21 @@ func main() {
 	}
 
 	defer conn.Close()
-
 	log.Printf("connection estableshd")
+	if err != nil {
+		log.Printf("%v", err)
+	}
 
-	dataFromWsConnection, err := io.ReadAll(conn)
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
 	if err != nil {
 		log.Printf("error while reading data from user: %v", err)
 
 		return
 	}
 
-	request := string(dataFromWsConnection)
-	fmt.Println(request)
-
-	lines := strings.Split(request, "\\")
+	request := string(buf)
+	lines := strings.Split(request, "/r/n")
 	if len(lines) == 0 {
 		log.Println("invalid request")
 
@@ -56,20 +54,11 @@ func main() {
 	}
 
 	parts := strings.Fields(lines[0])
-	if len(parts) != 3 {
-		log.Printf("invalid request")
-
-		return
-	}
-
-	path := parts[1]
-
-	path = strings.TrimLeft(path, "/")
-
-	content, err := os.ReadFile(path)
+	content, err := os.ReadFile(strings.TrimPrefix(parts[1], "/"))
 	if err != nil {
 		response := "HTTP/1.1 404 Not Found\r\n\r\nFile Not Found"
 		conn.Write([]byte(response))
+		log.Printf("can not read file: %v", err)
 
 		return
 	}
@@ -77,6 +66,8 @@ func main() {
 	response := "HTTP/1.1 200 OK\r\nContent-Length: " + fmt.Sprint(len(content)) + "\r\n\r\n" + string(content)
 	_, err = conn.Write([]byte(response))
 	if err != nil {
-		log.Printf("%v", err)
+		log.Printf("error while writing request: %v", err)
 	}
+
+	log.Printf("request procced successfully")
 }
