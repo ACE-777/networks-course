fe04bd227ddb89f8a11ae0b8476832cab84a3343
Mikhail Dyagilev <m.dyagilev@yadro.com>
2024-03-05 00:56:05 +0300

    done 3.3+done3.4

---
+++ lab03/lab03_C/cmd/ws_client/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	host := flag.String("server_host", "127.0.0.1", "address of host server")
+	port := flag.String("server_port", "9000", "ws server port")
+	file := flag.String("file", "files/example_file.txt", "path to request file")
+
+	flag.Parse()
+
+	conn, err := net.Dial("tcp", fmt.Sprintf("%v:%v", *host, *port))
+	if err != nil {
+		log.Printf("can not cnnect to host %v with port %v", *host, *port)
+	}
+
+	defer conn.Close()
+	log.Printf("succesfully connected to server on host: %v with port: %v via tcp", *host, *port)
+	request := fmt.Sprintf("GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", *file, fmt.Sprintf("%v:%v", *host, *port))
+	_, err = conn.Write([]byte(request))
+	if err != nil {
+		log.Printf("%v", err)
+	}
+
+	log.Printf("succesfully send new message")
+
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
+	if err != nil {
+		log.Printf("error while reading data from user: %v", err)
+
+		return
+	}
+
+	response := string(buf)
+	fmt.Println(response)
+}
---
+++ lab03/lab03_D/cmd/ws_server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	handler_request "lab_3/internal"
+)
+
+func main() {
+	port := flag.String("server_port", "9000", "ws server port")
+	concurrencyLevel := flag.Int("concurrency_level", 2, "max active connection")
+
+	flag.Parse()
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%v", *port))
+	if err != nil {
+		log.Printf("error while sw server starting: %v", err)
+
+		return
+	}
+
+	defer listener.Close()
+
+	log.Printf("Server is up on port%v", *port)
+
+	handler_request.Sem = make(chan struct{}, *concurrencyLevel)
+
+	for {
+		conn, err := listener.Accept()
+		log.Printf("accept connection")
+		if err != nil {
+			log.Printf("error while adding new connection: %v", err)
+
+			continue
+		}
+
+		handler_request.Sem <- struct{}{}
+
+		go handler_request.Handler(conn, handler_request.Sem)
+	}
+}
