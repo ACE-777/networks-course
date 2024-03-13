80bfa0757b15d52cf1a2288806735245dda5502c
Mikhail Dyagilev <m.dyagilev@yadro.com>
2024-03-13 11:06:23 +0300

    done A

--- lab04/server/internal/handler.go
+++ lab04/server/internal/handler.go
@@ -8,7 +8,8 @@ import (
 )
 
 const (
-	https = "https://"
+	https       = "https://"
+	contentType = "application/json"
 )
 
 var journal = make(map[string]int)
@@ -35,9 +36,43 @@ func (p *ProxyServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	url := strings.TrimPrefix(proxyRequest.URL.Path, "/")
-	resp, err := http.Get(https + url)
+	if r.Method == http.MethodGet {
+		resp, err := http.Get(https + url)
+		if err != nil {
+			http.Error(w, "Cant do GET", http.StatusInternalServerError)
+			log.Printf("can done get request, err: %v", err)
+
+			return
+		}
+
+		log.Printf("Response status code: %v\n", resp.Status)
+
+		for key, values := range resp.Header {
+			for _, value := range values {
+				w.Header().Add(key, value)
+			}
+		}
+
+		w.WriteHeader(resp.StatusCode)
+
+		journal[url] = resp.StatusCode
+
+		_, err = io.Copy(w, resp.Body)
+		if err != nil {
+			log.Printf("Error copying response body: %v\n", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+
+			return
+		}
+
+		log.Printf("success execute handler")
+
+		return
+	}
+
+	resp, err := http.Post(https+url, contentType, r.Body)
 	if err != nil {
-		http.Error(w, "Cant do GET", http.StatusInternalServerError)
+		http.Error(w, "Cant do POST", http.StatusInternalServerError)
 		log.Printf("can done get request, err: %v", err)
 
 		return
@@ -64,4 +99,6 @@ func (p *ProxyServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	log.Printf("success execute handler")
+
+	return
 }
